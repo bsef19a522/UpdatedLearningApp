9c02e7e3bf05ef77ef2cc317aad165c78a9ee742
bsef19a522 <bsef19a522@pucit.edu.pk>
2022-06-07 10:16:55 +0500

    Success

--- app/src/main/java/com/example/alearningapp/MainActivity.java
+++ app/src/main/java/com/example/alearningapp/MainActivity.java
@@ -11,8 +11,15 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String Alphabet[] = {"Apple","Ball","Cat","Dog"};
-    int img[] = {R.drawable.apple,R.drawable.ball,R.drawable.cat,R.drawable.dog};
+    String Alphabet[] = {"Apple","Ball","Cat","Dog","Egg","Fish","Goat","Hat","Ice-cream","Jar","Key"
+                            ,"Lamp","Map","Needle","Owl","Piano","Quilt","Rat","Sun","Tap","Umbrella"
+                            ,"Van","Watch","X-Ray","Yellow","Zebra"};
+    int img[] = {R.drawable.apple,R.drawable.ball,R.drawable.cat,R.drawable.dog,R.drawable.egg,
+                R.drawable.fish,R.drawable.goat,R.drawable.hat,R.drawable.ice,R.drawable.jar,
+                R.drawable.key,R.drawable.lamp,R.drawable.map,R.drawable.needle,R.drawable.owl,
+                R.drawable.piano,R.drawable.quilt,R.drawable.rat,R.drawable.sun,R.drawable.tap,
+                R.drawable.umbrella,R.drawable.van,R.drawable.watch,R.drawable.xray,
+                R.drawable.yellow,R.drawable.zebra};
 
     ListView lst;
 
